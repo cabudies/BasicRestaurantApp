61b8bd215eab6fc22d769de4d8f570c8727a1bbd
Gurjas Singh <gurjassingh92@gmail.com>
2018-07-21 10:35:49 +0530

    1. Added Spinner items. 2. Added ArrayList to add items of a spinner. 3. Create a new ArrayAdapter. 4. On button click, check the selected menu item and display it in a toast.

--- app/src/main/java/com/brillicaservices/gurjas/basicrestaurantapp/MainActivity.java
+++ app/src/main/java/com/brillicaservices/gurjas/basicrestaurantapp/MainActivity.java
@@ -3,16 +3,21 @@ package com.brillicaservices.gurjas.basicrestaurantapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText usernameText, userAge, quantity;
     Button submitOrder;
     Spinner menuSpinner;
+
+    ArrayList<String> spinnerItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +29,32 @@ public class MainActivity extends AppCompatActivity {
         submitOrder = findViewById(R.id.place_order);
         menuSpinner = findViewById(R.id.menu_spinner);
 
+        spinnerItems.add("Select Menu");
+        spinnerItems.add("Burger");
+        spinnerItems.add("Momos");
+        spinnerItems.add("Pizza");
+        spinnerItems.add("Mountain Dew");
+        spinnerItems.add("Hot Dog");
+
+        menuSpinner.setPrompt(spinnerItems.get(0));
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_dropdown_item, spinnerItems);
+
+        menuSpinner.setAdapter(arrayAdapter);
+
         submitOrder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String name = usernameText.getText().toString();
                 int age = Integer.parseInt(userAge.getText().toString());
                 int menuQuantity = Integer.parseInt(quantity.getText().toString());
+                String menuItem = menuSpinner.getSelectedItem().toString();
 
                 Toast.makeText(getApplicationContext(), "Username is: " + name +
                         "\nYour age is: " + age +
-                        "\nOrder quantity is: "+ menuQuantity,
+                        "\nOrder quantity is: "+ menuQuantity +
+                        "\nMenu name is: " + menuItem,
                         Toast.LENGTH_LONG).show();
             }
         });
